25cc9aa0e8f537b77055d5bb1dc3922100e36d8b
Osh <osh.pro@outlook.fr>
2021-03-25 20:59:36 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use std::path::Path;
 const FILE_PATH: &str = "./todo_list.txt";
 
 fn main() {
-
 	if !(Path::new(FILE_PATH).exists()) {
 		if let Err(err) = fs::File::create(FILE_PATH) {
 			eprint!("Error when creating file {}: {}", FILE_PATH, err);
 			process::exit(1);
 		}
 	}
-	
+
 	loop {
 		
 		println!("");
